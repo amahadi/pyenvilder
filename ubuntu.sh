5bc144fab5b9f4b81e4969f6cc0464783200cd89
alvi2496 <alvi.utsab@gmail.com>
2018-08-09 00:54:20 +0600

    initial start of script for ubuntu

---
+++ ubuntu.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
+source ~/.bashrc
