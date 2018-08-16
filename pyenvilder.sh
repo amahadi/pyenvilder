63ae482cda1cbdab0eda84727bf2b4738fbea309
alvi2496 <alvi.utsab@gmail.com>
2018-08-16 18:23:03 +0600

    add virtualenv installation

--- pyenvilder.sh
+++ pyenvilder.sh
@@ -1,18 +1,42 @@
 #!/bin/sh
 
-while getopts v:n: option
-do
-	case "${option}"
-	in
-		v) VERSION=${OPTARG};;
-		n) VENV_NAME=${OPTARG};;
-	esac
-done
+if [ -z "$1" ]; then 
+	echo "python version must be provided as -v option"
+elif [ -z "$2" ]; then 
+	echo "virtual environment name must be provided as -n option"
+else
+	PYENVILDER_PATH=`pwd`
 
-for i in {1..10}; do 
-	bash src/debian/pyenv.sh
-	if [ $? -eq 0 ]; then
-		pyenv install $VERSION
-		break
-	fi	 
-done
+	while getopts v:n: option
+	do
+		case "${option}"
+		in
+			v) VERSION=${OPTARG};;
+			n) VENV_NAME=${OPTARG};;
+		esac
+	done
+
+	for i in {1..10}; do 
+		bash $PYENVILDER_PATH/src/debian/pyenv.sh
+		if [ $? -eq 0 ]; then
+
+			pyenv install $VERSION
+
+			if [ ! -d ~/pyvirenvs ]; then
+				mkdir ~/pyvirenvs
+			fi	
+
+			cd ~/pyvirenvs
+			pyenv local $VERSION
+
+			for j in {1..10}; do
+				bash $PYENVILDER_PATH/src/debian/virtualenv.sh $VENV_NAME
+				if [ $? -eq 0 ]; then
+					break
+				fi
+			done
+			
+			break
+		fi	 
+	done
+fi
