1ed416839648be058d8dbabc65b97b231d65dc16
indrajit <eendroroy@gmail.com>
2018-08-17 16:41:31 +0600

    install pyenv deps

--- src/debian/pyenv.sh
+++ src/debian/pyenv.sh
@@ -1,3 +1,6 @@
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+	libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev
+
 if [ ! -d ~/.pyenv ]; then
 	git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
